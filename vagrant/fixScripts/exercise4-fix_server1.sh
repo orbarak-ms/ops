2cd6d7212cf704c79a962bb974b4fd45fa99f8de
orbarak-ms <46415241+orbarak-ms@users.noreply.github.com>
2019-08-11 17:02:38 +0300

    exercises

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
+sh -c 'echo "192.168.100.11 server2" >> /etc/hosts' && sleep 2
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+if [ "$#" -lt 2 ]; then
+    echo "illegal number of parameters"
+else
+	for last; do true; done
+	TARGET_FOLDER=${@: -1}
+	CURRENT_HOST=`hostname`
+	TOTAL_FILESIZE=0
+	if [ $CURRENT_HOST == "server1" ]; then
+		TARGET_HOST=server2
+	else
+		TARGET_HOST=server1
+	fi
+	for FILE in "$@"; do
+		if [ -e $FILE ]; then
+			if [ $FILE != $TARGET_FOLDER ]; then
+				FILESIZE=$(stat -c%s "$FILE")
+				TOTAL_FILESIZE=$((TOTAL_FILESIZE+FILESIZE))
+				scp -q $FILE $TARGET_HOST:$TARGET_FOLDER  
+			fi
+		fi
+	done
+	echo $TOTAL_FILESIZE
+fi
